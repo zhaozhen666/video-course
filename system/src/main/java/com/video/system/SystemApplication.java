f93f3e7bfcbc7ea3e66adf75caec438aa9ac511b
zhaozhen <351848327@qq.com>
2022-10-25 07:48:29 +0800

    mybatis

--- system/src/main/java/com/video/system/SystemApplication.java
+++ system/src/main/java/com/video/system/SystemApplication.java
@@ -1,11 +1,13 @@
 package com.video.system;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@MapperScan("com.video.system.mapper")
 public class SystemApplication {
 
     public static void main(String[] args) {
